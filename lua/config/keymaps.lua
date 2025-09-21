6c21a537721ef2d506843844ebb38b6a2275b0a8
Dzikry <dzikrynp14@gmail.com>
2025-09-21 15:05:46 +0700

    conf

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,19 @@ vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+-- Moving lines
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Centered scrolling
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+
+-- Centered searching
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Clear highlight
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
