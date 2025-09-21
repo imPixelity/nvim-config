7134ea41ad0805c74dc3faf1eeed863d94649ee5
Dzikry <dzikrynp14@gmail.com>
2025-09-21 14:43:25 +0700

    initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,38 @@
+-- Line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Indentation
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- Search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+-- UI
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+vim.opt.signcolumn = "yes"
+vim.opt.termguicolors = true
+vim.opt.wrap = false
+
+-- Splits
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- Files & Undo
+vim.opt.hidden = true
+vim.opt.undofile = true
+vim.opt.backup = false
+vim.opt.swapfile = false
+
+-- Behavior
+vim.opt.clipboard = "unnamedplus"
+vim.opt.updatetime = 50
+vim.opt.mouse = "a"
