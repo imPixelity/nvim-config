ccedf66dd3b402f1805be06252278455e51e050d
Dzikry <dzikrynp14@gmail.com>
2025-09-21 21:22:03 +0700

    bufferline

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,13 @@
+return {
+    {'akinsho/bufferline.nvim',
+        version = "*", 
+        dependencies = 'nvim-tree/nvim-web-devicons',
+        config = function()
+            require("bufferline").setup{}
+            vim.keymap.set("n", "<S-l>", ":BufferLineCycleNext<CR>")
+            vim.keymap.set("n", "<S-h>", ":BufferLineCyclePrev<CR>")
+            vim.keymap.set("n", "<leader>bd", ":bdelete<CR>")
+        end
+    }
+}
+
