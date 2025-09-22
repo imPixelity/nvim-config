30ca26a97cd2a9478d25e2a4fb5cdb1a7ab3eaf8
Dzikry <dzikrynp14@gmail.com>
2025-09-22 11:33:25 +0700

    stuff

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,6 +8,7 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
+vim.opt.autoindent = true
 
 -- Search
 vim.opt.ignorecase = true
@@ -18,9 +19,14 @@ vim.opt.incsearch = true
 -- UI
 vim.opt.cursorline = true
 vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 8
 vim.opt.signcolumn = "yes"
+vim.opt.colorcolumn = "100"
 vim.opt.termguicolors = true
 vim.opt.wrap = false
+vim.opt.showmatch = true
+vim.opt.matchtime = 2
+vim.opt.showmode = false
 
 -- Splits
 vim.opt.splitbelow = true
@@ -33,6 +39,7 @@ vim.opt.backup = false
 vim.opt.swapfile = false
 
 -- Behavior
+vim.opt.errorbells = false
 vim.opt.clipboard = "unnamedplus"
 vim.opt.updatetime = 50
 vim.opt.mouse = "a"
